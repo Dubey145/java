e29f34bd638ee178d696b84be6847ea96b3a3bec
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-07-17 05:52:13 +0530

    updated a getter and setter

--- OOPS/Box.java
+++ OOPS/Box.java
@@ -1,11 +1,31 @@
 public class Box {
-    int length;
-    int breadth;
-    int depth;
+    private int length;
+    private int breadth;
+    private int depth;
     String boxName;
     String color;
 
-    public static void desc(){
-        //System.out.println("i am " + this.boxName + "  and i am " + this.color); 
+    //adding methods
+
+    public void desc(){
+        System.out.println("i am " + this.boxName + "  and i am " + this.color + " with length " + this.length ); 
+    }
+    //constructor
+    
+    public Box(int length, int breadth, int depth){
+        this.length = length;
+        this.depth = depth;
+        this.boxName = null;
+        this.color = null;
+        this.breadth = breadth;
+        //System.out.println("called from cube");
+    }
+    //private members are updated with getters and setters
+    //getter 
+    public int getLength(){
+        return this.length;
+    }
+    public void setLength(int length) {
+        this.length = length;
     }
 }
