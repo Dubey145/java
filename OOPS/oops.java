d8edc8bc33a09b6508cb8ad1648075d3c187935e
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-07-17 05:51:27 +0530

    Add files via upload

--- OOPS/oops.java
+++ OOPS/oops.java
@@ -1,6 +1,6 @@
 import java.util.*;
 //importing box class
- 
+
 public class oops{ 
     //this is a class
 
@@ -9,14 +9,30 @@ public class oops{
         //class is a template for the object
         //check class box defined in this folder
 
-        Box cube = new Box();
-        cube.length = 10;
-        cube.depth = 10;
-        cube.breadth = 10;
-        cube.boxName = "first cube in the world";
-        cube.color = "white";
-        System.out.println(cube.length);
+        Box cuboid = new Box(20,10,10);
+        //cube.length = 10;
+        //cube.depth = 10;
+        //cube.breadth = 10;
+        cuboid.boxName = "THE first cuboid in the world";
+        cuboid.color = "blue";
+        System.out.println(cuboid.getLength());
+        cuboid.desc();
+        cuboid.setLength(25);
+        cuboid.desc();
 
+        //creating a class object of cube that inherits box
+
+        cube newCube = new cube(265);
+        newCube.description();
+        //System.out.println(newCube.side); cant access as side is private
+       cube secondCube = new cube();
     }
     
 }
+/*
+--The Java Class Library-- 
+in java we have a lot of classes available to us in different packages
+example String, StringBuilder, System, Math etc 
+
+https://docs.oracle.com/javase/8/docs/api/
+*/
